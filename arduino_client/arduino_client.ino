c44f03f5fe2d7bb8b45aea8d6700c39733914b6a
Autophagy <mika@crate.io>
2018-04-07 22:12:48 +0200

    Convert station code from arduino to ESP8266

--- arduino_client/arduino_client.ino
+++ arduino_client/arduino_client.ino
@@ -20,7 +20,7 @@
 #include <Adafruit_BMP085_U.h>
 
 // Constants
-#define DHTPIN 2 // DHT22 connected to pin 2
+#define DHTPIN 12 // DHT22 connected to pin 12
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_LOW, 12345);
@@ -29,10 +29,8 @@ Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 
 // Variables
 bool DEBUG = true; // Set to true to output sensor information to the Serial port.
-int pollingLEDPin = 4;
 
-long sensor_interval = 1000; // 1 second
-int polling_amount = 20; // Number of times to poll the sensors before averaging
+long sensor_interval = 10000; // 10 seconds
 float temperature;
 float humidity;
 float pressure;
@@ -41,43 +39,26 @@ float lux;
 void setup() {
 
   if (DEBUG) {
-    Serial.begin(9600);
+    Serial.begin(115200);
   }
 
   dht.begin();
   tsl.enableAutoRange(true);
   tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);
   bmp.begin();
-
-  pinMode(pollingLEDPin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(pollingLEDPin, HIGH);
-
-  float temp_sum = 0.0;
-  float humidity_sum = 0.0;
-  float pressure_sum = 0.0;
-  float lux_sum = 0.0;
-
-  for (int i=0; i < polling_amount; i++){
-    temp_sum += getTemperature();
-    humidity_sum += getHumidity();
-    pressure_sum += getPressure();
-    lux_sum += getLux();
-  }
-
-  temperature = temp_sum/polling_amount;
-  humidity = humidity_sum/polling_amount;
-  pressure = pressure_sum/polling_amount;
-  lux = lux_sum/polling_amount;
+  temperature = getTemperature();
+  humidity = getHumidity();
+  pressure = getPressure();
+  lux = getLux();
 
   if (DEBUG) {
     outputDebugInfo();
   }
 
   // Delay by sensor_interval for each read
-  digitalWrite(pollingLEDPin, LOW);
   delay(sensor_interval);
 }
 
