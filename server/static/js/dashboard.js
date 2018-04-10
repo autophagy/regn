62c99700a910cfad8f465e88717fc425381a0f46
Autophagy <mika@crate.io>
2018-04-10 22:41:37 +0200

    Genericise js into a dashboard object

--- server/static/js/dashboard.js
+++ server/static/js/dashboard.js
@@ -1,16 +1,97 @@
-var graph_request = new XMLHttpRequest();
-graph_request.open('GET', '/api/temperature/day', true);
-var latest_request = new XMLHttpRequest();
-latest_request.open('GET', '/api/latest', true);
+class Dashboard {
+    constructor() {
+        this.defaultSensorType = 'temperature'
+        this.defaultGranularity = 'day'
 
-var scatter
+        this.sensorTypes = Object.freeze({
+            'temperature': {
+                'apiTerm': 'temperature',
+                'title': 'Temperature',
+                'unit': '°C',
+                'suggestedMin': -5,
+                'suggestedMax': 25,
+            },
+            'humidity': {
+                'apiTerm': 'humidity',
+                'title': 'Humidity',
+                'unit': '%',
+                'suggestedMin': 0,
+                'suggestedMax': 100,
+            },
+            'pressure': {
+                'apiTerm': 'pressure',
+                'title': 'Pressure',
+                'unit': ' hPa',
+                'suggestedMin': 900,
+                'suggestedMax': 1100,
+            },
+            'luminosity': {
+                'apiTerm': 'luminosity',
+                'title': 'Luminosity',
+                'unit': ' lux',
+                'suggestedMin': 0,
+                'suggestedMax': 1500,
+            },
+        });
 
-graph_request.onload = function() {
-    if (graph_request.status >= 200 && graph_request.status < 400) {
-        var dat = JSON.parse(graph_request.responseText);
+        this.granularities = Object.freeze({
+            'day': {
+                'apiTerm': 'day',
+                'title': 'Day',
+            },
+            'week': {
+                'apiTerm': 'week',
+                'title': 'Week',
+            },
+            'month': {
+                'apiTerm': 'month',
+                'title': 'Month',
+            },
+            'year': {
+                'apiTerm': 'year',
+                'title': 'Year',
+            },
+        });
+
+        this.lastUpdated = new Date().getTime();
+        this.mode = this.sensorTypes[this.defaultSensorType];
+        this.granularity = this.granularities[this.defaultGranularity];
+        this.refresh_latest();
+        this.init_dashboard();
+    }
+
+    init_dashboard() {
+        var graph_request = new XMLHttpRequest();
+        graph_request.open('GET', '/api/' + this.mode.apiTerm + '/' + this.granularity.apiTerm, true);
+        var self = this
+        graph_request.onload = function() {
+            if (graph_request.status >= 200 && graph_request.status < 400) {
+                self.create_new_graph(JSON.parse(graph_request.responseText))
+            }
+        }
+        graph_request.send();
+    }
+
+    refresh_latest() {
+        var latest_request = new XMLHttpRequest();
+        latest_request.open('GET', '/api/latest', true);
+        var self = this
+        latest_request.onload = function() {
+            if (latest_request.status >= 200 && latest_request.status < 400) {
+                var dat = JSON.parse(latest_request.responseText);
+                for (var key in self.sensorTypes) {
+                    document.getElementById("latest-" + self.sensorTypes[key].apiTerm).innerHTML = dat[self.sensorTypes[key].apiTerm] + self.sensorTypes[key].unit
+                    document.getElementById("latest-" + self.sensorTypes[key].apiTerm + "-title").innerHTML = self.sensorTypes[key].title
+                }
+            }
+        };
+        latest_request.send();
+    }
+
+    create_new_graph(data) {
         var scatterChartData = {
             datasets: [{
-                data: dat.map(x => ({
+                data: data.map(x => ({
                     "x": new Date(x["timestamp"]),
                     "y": x["value"]
                 })),
@@ -76,8 +157,8 @@ graph_request.onload = function() {
                             labelString: 'y axis'
                         },
                         ticks: {
-                            suggestedMin: -5,
-                            suggestedMax: 25,
+                            suggestedMin: this.mode.suggestedMin,
+                            suggestedMax: this.mode.suggestedMax,
                             fontColor: '#e6e6e6',
                             fontFamily: 'Inconsolata',
                             padding: 20
@@ -85,23 +166,16 @@ graph_request.onload = function() {
                     }]
                 }
             }
+        };
+
+        if (this.graph == null) {
+            var ctx = document.getElementById("canvas").getContext("2d");
+            this.graph = Chart.Scatter(ctx, chartOptions);
+        } else {
+            this.graph.options = chartOptions;
+            this.graph.update();
         }
-
-        var ctx = document.getElementById("canvas").getContext("2d");
-        window.myScatter = Chart.Scatter(ctx, chartOptions);
-
     }
-};
+}
 
-latest_request.onload = function() {
-    if (latest_request.status >= 200 && latest_request.status < 400) {
-        var dat = JSON.parse(latest_request.responseText);
-        document.getElementById("latest-temperature").innerHTML = dat['temperature'] + "°C"
-        document.getElementById("latest-humidity").innerHTML = dat['humidity'] + "%"
-        document.getElementById("latest-pressure").innerHTML = dat['pressure'] + " hPa"
-        document.getElementById("latest-luminosity").innerHTML = dat['luminosity'] + " lux"
-    }
-};
-
-graph_request.send();
-latest_request.send();
+var dashboard = new Dashboard();
